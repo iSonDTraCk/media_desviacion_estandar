5c972a65a3bd6dfd1a2dfd56a577fa513c93b2f2
david <73234956@continental.edu.pe>
2024-10-21 20:13:44 -0500

    Caso con n elementos, donde hay elementos que no son numéricos (esto debe generar una excepción TypeError).

--- src/calculadora.py
+++ src/calculadora.py
@@ -4,5 +4,8 @@ class NoSePuedeCalcular(Exception):
 def calcular_media(elementos):
     if not elementos:
         raise NoSePuedeCalcular("La lista está vacía")
+    if not all(isinstance(e, (int, float)) for e in elementos):
+        raise TypeError("Todos los elementos deben ser números")
     return sum(elementos) / len(elementos)
+
 
