19b65fe7caff9bb4aa0ace9ec6c4dafe2b65f9ba
david <73234956@continental.edu.pe>
2024-10-21 20:11:00 -0500

    Caso con n elementos donde todos son ceros.

--- src/calculadora.py
+++ src/calculadora.py
@@ -4,4 +4,5 @@ class NoSePuedeCalcular(Exception):
 def calcular_media(elementos):
     if not elementos:
         raise NoSePuedeCalcular("La lista está vacía")
-    return sum(elementos) / len(elementos)
+    return 0 if all(e == 0 for e in elementos) else sum(elementos) / len(elementos)
+
