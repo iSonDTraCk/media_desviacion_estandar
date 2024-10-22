77fdacf7af5e6e19e884f0af5a2ce3aea6bad7b7
david <73234956@continental.edu.pe>
2024-10-21 20:02:57 -0500

    Caso sin elementos en la lista debe lanzar una excepción NoSePuedeCalcular, la excepción se debe crear.

---
+++ src/calculadora.py
@@ -0,0 +1,7 @@
+class NoSePuedeCalcular(Exception):
+    pass
+
+def calcular_media(elementos):
+    if not elementos:
+        raise NoSePuedeCalcular("La lista está vacía")
+    return sum(elementos) / len(elementos)
