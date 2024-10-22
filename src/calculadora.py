5a1c7a6467104519ee16a33f47d4308cc6592411
david <73234956@continental.edu.pe>
2024-10-21 20:11:59 -0500

    Caso con n elementos positivos y negativos.

--- src/calculadora.py
+++ src/calculadora.py
@@ -4,5 +4,5 @@ class NoSePuedeCalcular(Exception):
 def calcular_media(elementos):
     if not elementos:
         raise NoSePuedeCalcular("La lista está vacía")
-    return 0 if all(e == 0 for e in elementos) else sum(elementos) / len(elementos)
+    return sum(elementos) / len(elementos)
 
